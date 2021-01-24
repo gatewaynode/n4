5e37c87724b561de402a867ca0bb22630284d6c1
Gatewaynode <44749714+gatewaynode@users.noreply.github.com>
2021-01-24 15:09:22 -0500

    Cleanup and refactor for config file almost finished.

--- src/lib.rs
+++ src/lib.rs
@@ -19,34 +19,97 @@ use serde_derive::{Deserialize, Serialize};
 use serde_json;
 use v_htmlescape::escape;
 
-// Currently a development dependency
+// Currently a development dependency, probably going to move to a module
 use file_tree::*;
 
-// TODO Temporarily hard coded, move to config file
+/// Struct to hold the site configuration
+///
+/// prod_host
+///     sitemap-data: Production protocol and FQDN such as https://slashdot.org/
+/// xml_priority: String
+///     sitemap-data: Just 0.64 normally for sitemap.xml
+/// base_dir
+///     content-data: Relative root directory name of the content
+/// local_content_dir
+///     content-data: Absolute path to content directory, concatenated with base dir on end
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SiteConfig {
-    prod_host: String, // Production protocol and FQDN such as https://gatewaynode.com
-    xml_priority: String, // Just 0.64 normally for sitemap.xml
-    base_dir: String,  // Relative root directory name of the content
-    local_content_dir: String, // Absolute path to content directory, concatenated with base dir on end
+    pub prod_host: String,
+    pub xml_priority: String,
+    pub base_dir: String,
+    pub local_content_dir: String,
 }
 
-impl Default for SiteConfig {
-    fn default() -> Self {
-        SiteConfig {
-            prod_host: String::from("https://gatewaynode.com"),
-            xml_priority: String::from("0.64"),
-            base_dir: String::from("website/"),
-            local_content_dir: String::from("/home/anon/Documents/gatewaynode_notes/"),
-        }
+impl SiteConfig {
+    pub fn local_path(self) -> String {
+        format!("{}{}", self.local_content_dir, self.base_dir)
     }
 }
 
 pub fn load_config() -> SiteConfig {
-    let config_dir = dirs::config_dir();
-    dbg!(config_dir);
-    let site_config = SiteConfig::default();
-    site_config
+    let mut site_config = String::new();
+    let mut config_file_path: PathBuf = match dirs::config_dir() {
+        Some(val) => PathBuf::from(val),
+        _ => panic!("No system config value found!"),
+    };
+    config_file_path.push("n4");
+    config_file_path.push("default.json");
+    // File read
+    let mut _file = match fs::File::open(&config_file_path) {
+        Err(why) => panic!("Couldn't open config file: {}", why),
+        Ok(mut _file) => _file.read_to_string(&mut site_config),
+    };
+    // Deserialize the JSON
+    let deserialized_site_config: SiteConfig = match serde_json::from_str(&site_config) {
+        Err(why) => panic!("Config couldn't be deserialized: {}", why),
+        Ok(value) => value,
+    };
+    deserialized_site_config
+}
+
+/// Creates the standard user config directory and an empty config JSON file
+pub fn setup_config() {
+    let mut config_dir: PathBuf = match dirs::config_dir() {
+        Some(val) => PathBuf::from(val),
+        _ => panic!("No system config value found!"),
+    };
+
+    config_dir.push("n4");
+    if config_dir.exists() {
+        println!(
+            "Config dir exists!  Edit the file at {}/default.json",
+            &config_dir.to_string_lossy()
+        )
+    } else {
+        match std::fs::create_dir(&config_dir) {
+            Err(why) => panic!("Directory couldn't be created: {}", why),
+            _ => println!("Created!"),
+        };
+    }
+
+    config_dir.push("default.json");
+    if config_dir.exists() {
+        panic!("Default config already exists.  Exiting.");
+    } else {
+        let default_config = SiteConfig {
+            prod_host: String::from("https://localhost:8000"),
+            xml_priority: String::from("0.64"),
+            base_dir: String::from("/"),
+            local_content_dir: String::from("/"),
+        };
+        let mut file = match fs::File::create(config_dir) {
+            Err(why) => panic!("File creation fail: {}", why),
+            Ok(value) => value,
+        };
+        let serialized_config = match serde_json::to_string_pretty(&default_config) {
+            Err(why) => panic!("Serialize to json fail: {}", why),
+            Ok(value) => value,
+        };
+        match file.write_all(&serialized_config.as_bytes()) {
+            Err(why) => panic!("Could not write to config file: {}", why),
+            Ok(_) => println!("Default config file created."),
+        };
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, Default)]
@@ -113,11 +176,6 @@ impl Default for ContentMeta {
     }
 }
 
-// impl ContentMeta {
-//     pub fn as_string(Self) -> String {
-//     }
-// }
-
 #[derive(Serialize, Deserialize, Debug)]
 pub struct DirContent {
     modified: chrono::DateTime<chrono::Utc>, //NaiveDateTime,
@@ -172,6 +230,17 @@ impl Default for MenuItemMeta {
     }
 }
 
+pub fn generate_robot_food() -> String {
+    let config = load_config();
+    format!(
+        "User-agents: *
+Allow: *
+
+Sitemap: {}/sitemap.xml",
+        config.prod_host
+    )
+}
+
 // TODOget back to this once we have the extensions in the file meta from file_tree
 // fn count_md_files(regular_file_list: HashMap<String, file_tree::FileMeta>) -> u32 {
 //    0
@@ -199,23 +268,22 @@ pub fn read_menu_meta_file(file_path: PathBuf) -> MenuItemMeta {
 
 // Formats a path to a directory for the .menu_meta extension and checks if it exists
 pub fn add_menu_metadata(meta_path_raw: &String) -> MenuItemMeta {
-    let meta_path: PathBuf = PathBuf::from(&format!(
-        "{}{}",
-        meta_path_raw, //.strip_suffix("/").unwrap_or(),
-        ".menu_meta"
-    ));
+    let meta_path: PathBuf = PathBuf::from(&format!("{}{}", meta_path_raw, ".menu_meta"));
 
     if meta_path.exists() {
-        // return MenuItemMeta::default();
         read_menu_meta_file(meta_path)
     } else {
         return MenuItemMeta::default();
     }
 }
 
-// TODO pass in prefix to strip from site config
 pub fn tree_to_menus(dir_tree: DirTree) -> HashMap<String, MenuItem> {
     let mut menus: HashMap<String, MenuItem> = HashMap::new();
+    let config = load_config();
+    let prefix_to_strip = match config.base_dir.strip_suffix("/") {
+        Some(val) => val,
+        _ => panic!("Base dir is missing the trailing directory delimiter."),
+    };
     for (key, value) in dir_tree.directories {
         if value.directories.len() > 0 {
             menus.insert(
@@ -225,10 +293,10 @@ pub fn tree_to_menus(dir_tree: DirTree) -> HashMap<String, MenuItem> {
                     number_of_files: value.files.len() as u32,
                     relative_path: value
                         .relative_path
-                        .strip_prefix("website") // TODO
+                        .strip_prefix(prefix_to_strip)
                         .unwrap()
                         .to_string(),
-                    children: tree_to_menus(value),
+                    children: tree_to_menus(value), // Recursion
                 },
             );
         } else {
@@ -239,10 +307,10 @@ pub fn tree_to_menus(dir_tree: DirTree) -> HashMap<String, MenuItem> {
                     number_of_files: value.files.len() as u32,
                     relative_path: value
                         .relative_path
-                        .strip_prefix("website") // TODO
+                        .strip_prefix(prefix_to_strip)
                         .unwrap()
                         .to_string(),
-                    children: HashMap::new(),
+                    children: HashMap::new(), // Blank default
                 },
             );
         }
@@ -259,7 +327,7 @@ pub fn unix_time_to_iso(timestamp: f64) -> chrono::DateTime<chrono::Utc> {
 }
 
 fn tree_to_sitemap(dir_tree: DirTree) -> Vec<SiteMapEntry> {
-    let config = SiteConfig::default();
+    let config = load_config();
     let mut files: Vec<SiteMapEntry> = Vec::new();
 
     if dir_tree.files.len() > 0 {
@@ -316,7 +384,8 @@ fn tree_to_sitemap(dir_tree: DirTree) -> Vec<SiteMapEntry> {
 }
 
 pub fn generate_sitemap() -> Vec<SiteMapEntry> {
-    let dir_tree = file_tree::dir_to_tree("/home/anon/Documents/gatewaynode_notes/website", "");
+    let config = load_config();
+    let dir_tree = file_tree::dir_to_tree(&config.local_path(), "");
 
     let sitemap = tree_to_sitemap(dir_tree);
 
@@ -324,74 +393,61 @@ pub fn generate_sitemap() -> Vec<SiteMapEntry> {
 }
 
 pub fn generate_content_state() -> file_tree::DirTree {
-    let dir_tree = file_tree::dir_to_tree("/home/anon/Documents/gatewaynode_notes/website", "");
+    let config = load_config();
+    let dir_tree = file_tree::dir_to_tree(&config.local_path(), "");
     dir_tree
 }
 
-pub fn read_md_dirs(dir: &str, rel_path: &str) -> Vec<DirContent> {
-    let paths = fs::read_dir(dir).unwrap();
-    let mut contents: Vec<DirContent> = Vec::new();
-
-    for item in paths {
-        let this_path = &item.unwrap().path();
-        if !this_path.is_dir() && this_path.extension().unwrap() == "md" {
-            let new_content = DirContent {
-                modified: read_file_modified_time(&this_path),
-                title: String::from(this_path.file_stem().unwrap().to_string_lossy()),
-                relative_path: String::from(rel_path),
-            };
-            contents.push(new_content);
-        } else if this_path.is_dir() {
-            let dir_name: String = String::from(this_path.file_stem().unwrap().to_string_lossy());
-            let new_rel_path: String = format!("{}/{}", rel_path, dir_name);
-            // Recursive call here
-            contents.append(&mut read_md_dirs(
-                &this_path.to_string_lossy(),
-                &new_rel_path,
-            ));
-        }
-    }
-    contents.sort_unstable_by_key(|x| x.modified); // Not sure if this needs to be sorted this way now
-                                                   // println!("{:?}", contents);
-    contents
-}
-
 fn localpath_to_webpath(this_path: &std::path::PathBuf) -> String {
-    let config = SiteConfig::default();
+    let config = load_config();
     let mut rel_path = this_path.to_string_lossy().to_string();
+    // offset is necessary for replace range, this is the calculation of it
     let offset = rel_path.find(&config.base_dir).unwrap() + config.base_dir.len(); // I think panic here is ok as it will break the site generally
     rel_path.replace_range(..offset, "/");
     rel_path.strip_suffix(".md").unwrap().to_string()
 }
 
 // TODO Change the content read to use the single page read
-pub fn read_full_dir_sorted(dir: &str) -> Vec<PageContent> {
-    let paths = fs::read_dir(dir).unwrap();
+pub fn read_full_dir_sorted(raw_dir: String) -> Vec<PageContent> {
+    let config = load_config();
+    let content_dir: String = format!("{}{}{}", config.local_content_dir, config.base_dir, raw_dir);
+    let paths = match fs::read_dir(&content_dir) {
+        Err(why) => panic!("Dir exists but can't be read: {}", why),
+        Ok(val) => val,
+    };
     let mut pages: HashMap<String, PageContent> = HashMap::new();
 
+    let prefix = config.local_path();
     for item in paths {
-        let this_path = &item.unwrap().path(); // Just easier this way
-        if !&this_path.is_dir() {
-            let file_stem: String = String::from(this_path.file_stem().unwrap().to_string_lossy());
+        let this_path = match &item {
+            Err(why) => panic!("Bad item found in directory list: {}", why),
+            Ok(val) => val.path(),
+        };
+        // Only try to add content files, ignore directories and metadata files
+        if !&this_path.is_dir()
+            && !this_path
+                .extension()
+                .unwrap()
+                .to_string_lossy()
+                .to_string()
+                .ends_with("meta")
+        {
+            // Need the file stem
+            let file_stem: String = match this_path.file_stem() {
+                Some(val) => val.to_string_lossy().to_string(),
+                _ => panic!("Couldn't find file stem in directory listing"),
+            };
+            // Need just the web relative path
+            let dir_item_rel_path = match this_path
+                .to_string_lossy()
+                .to_string()
+                .strip_prefix(&prefix)
+            {
+                Some(val) => val.to_string(),
+                _ => panic!("Local content path prefix doesn't exist!?!  This is strange."),
+            };
             if !pages.contains_key(&file_stem) {
-                pages.insert(file_stem.clone(), PageContent::default());
-            }
-            if this_path.extension().unwrap() == "md" {
-                let mut new_content = MDContent {
-                    created: read_file_creation_time(&this_path),
-                    title: file_stem.clone(),
-                    path: localpath_to_webpath(this_path),
-                    body: read_markdown_from_path(&this_path),
-                    list: Vec::new(),
-                    meta: ContentMeta::default(),
-                };
-                // Check for metadata and load that if it exists
-                if check_path_alternatives(this_path, ".content_meta") {
-                    let content_meta_path =
-                        String::from(this_path.to_string_lossy()).replace(".md", ".content_meta");
-                    new_content.meta = read_content_meta_file(PathBuf::from(content_meta_path));
-                }
-                pages.get_mut(&file_stem).unwrap().markdown = new_content;
+                pages.insert(file_stem.clone(), read_single_page(dir_item_rel_path));
             }
         }
     }
@@ -407,13 +463,15 @@ pub fn read_full_dir_sorted(dir: &str) -> Vec<PageContent> {
 
 // Mainly for reading the content_meta content_list values prefixes local dir and document base dir
 pub fn read_content_list(list_o_content: &Vec<String>) -> Vec<PageContent> {
-    let config = SiteConfig::default();
+    let config = load_config();
     let mut page_list: Vec<PageContent> = Vec::new();
+    let content_path = config.local_path();
     for item in list_o_content {
-        let temp_string = format!("{}{}{}", config.local_content_dir, config.base_dir, item);
+        let temp_string = format!("{}{}", &content_path, item);
         let this_path = PathBuf::from(temp_string);
+        // Is this a path traversal error?
         if !&this_path.is_dir() && this_path.exists() {
-            page_list.push(read_single_page(this_path.as_path()));
+            page_list.push(read_single_page(item.clone()));
         } else {
             println!("Content list failure.");
             dbg!(list_o_content);
@@ -424,23 +482,28 @@ pub fn read_content_list(list_o_content: &Vec<String>) -> Vec<PageContent> {
     page_list
 }
 
-pub fn read_single_page(this_path: &std::path::Path) -> PageContent {
+// pub fn read_single_page(this_path: &std::path::Path) -> PageContent {
+pub fn read_single_page(this_path: String) -> PageContent {
+    let config = load_config();
+    let this_path = PathBuf::from(format!("{}{}", config.local_path(), this_path));
     let mut page_content: PageContent = PageContent::default();
-    let file_stem: String = String::from(this_path.file_stem().unwrap().to_string_lossy());
-
+    let file_stem: String = match this_path.file_stem() {
+        Some(val) => val.to_string_lossy().to_string(),
+        _ => panic!("Couldn't find file stem while reading single page."),
+    };
     // Ensure markdown extension and convert markdown to HTML string
     if this_path.extension().unwrap() == "md" {
         page_content.markdown = MDContent {
             created: read_file_creation_time(&this_path),
             title: file_stem,
-            path: localpath_to_webpath(&PathBuf::from(this_path)), //this_path.to_string_lossy().to_string().clone(),
+            path: localpath_to_webpath(&PathBuf::from(&this_path)),
             body: read_markdown_from_path(&this_path),
             list: Vec::new(),
             meta: ContentMeta::default(),
         };
     }
     // Check for content metadata and load that if it exists
-    if check_path_alternatives(this_path, ".content_meta") {
+    if check_path_alternatives(&this_path, ".content_meta") {
         let content_meta_path =
             String::from(this_path.to_string_lossy()).replace(".md", ".content_meta");
         page_content.markdown.meta = read_content_meta_file(PathBuf::from(content_meta_path));
@@ -479,13 +542,40 @@ pub fn read_content_meta_file(file_path: PathBuf) -> ContentMeta {
 // This function looks for a base markdown file by extension and returns TRUE if it exists, confirming there is a piece
 // of content that inherits CSS or JSON
 // TODO Add an input validation layer here, check for illegal escape attempts and return False if found
-pub fn check_path_alternatives(this_path: &Path, extension: &str) -> bool {
+pub fn check_path_alternatives(this_path: &PathBuf, extension: &str) -> bool {
     let path_check_str: String = this_path.to_string_lossy().replace(".md", extension);
     let new_path: &Path = Path::new(&path_check_str);
     new_path.exists()
 }
 
-// TODO Deprecate in favor of file_tree data
+// INFO Potential section of file operations to move to a module
+
+pub fn does_content_exist(potential_content: String) -> bool {
+    let config = load_config();
+    // TODO Insert additional path traversal filters here
+    let raw_path = format!("{}{}", config.local_path(), potential_content,);
+    // Markdown content test
+    if Path::new(&format!("{}.{}", raw_path, "md")).exists() {
+        return true;
+    } else {
+        // TODO "else if" here to check for .json and .html and such
+        return false;
+    }
+}
+
+pub fn does_directory_exist(potential_content: String) -> bool {
+    let config = load_config();
+    // Insert additional path traversal filters here
+    let raw_path = format!("{}{}", config.local_path(), potential_content,);
+    // Markdown content test
+    if Path::new(&raw_path).is_dir() {
+        return true;
+    } else {
+        // Maybe a good place for a directory blacklist?
+        return false;
+    }
+}
+
 pub fn read_file_creation_time(path: &std::path::Path) -> chrono::DateTime<chrono::Utc> {
     //NaiveDateTime {
     let metadata = fs::metadata(path).expect("Not found");
@@ -499,13 +589,12 @@ pub fn read_file_creation_time(path: &std::path::Path) -> chrono::DateTime<chron
     };
 }
 
-// TODO Deprecate in favor of file_tree data
 pub fn read_file_modified_time(path: &std::path::Path) -> chrono::DateTime<chrono::Utc> {
     //NaiveDateTime {
     let metadata = fs::metadata(path).expect("Not found");
 
     let _ = match metadata.modified() {
-        Err(why) => panic!("Couldn't get file metadata: {}", why),
+        Err(why) => panic!("Couldn't get file metadata: {}", why), // TODO Remove panic
         Ok(_time) => {
             let _temp_time = _time.duration_since(UNIX_EPOCH).unwrap().as_secs() as f64;
             return unix_time_to_iso(_temp_time); //NaiveDateTime::from_timestamp(_temp_time, 0);
